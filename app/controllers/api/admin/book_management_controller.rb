8083fe886cefd9cc1c3f45611eb6e14bd4c586c9
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-12-24 12:54:56 +0700

    feat: unify book request body schema

--- app/controllers/api/admin/book_management_controller.rb
+++ app/controllers/api/admin/book_management_controller.rb
@@ -9,35 +9,26 @@ class Api::Admin::BookManagementController < ApplicationController
   end
 
   def create
-    begin
-      book = Book.create(
-        title: create_book_params[:title],
-        publication_year: create_book_params[:publication_year],
-        isbn: create_book_params[:isbn],
-        book_cover: create_book_params[:book_cover],
-        cover_file_name: create_book_params[:cover_file_name],
-        original_stock: create_book_params[:original_stock],
-        current_stock: create_book_params[:current_stock],
-        description: create_book_params[:description],
-        book_type: create_book_params[:book_type],
-        price: create_book_params[:price],
-        author_id: create_book_params[:author_id],
-        category_id: create_book_params[:category_id],
-        genre_id: create_book_params[:genre_id],
-        language_id: create_book_params[:language_id])
+    book = Book.create(
+      title: create_params[:title],
+      publication_year: create_params[:publication_year],
+      isbn: create_params[:isbn],
+      book_cover: create_params[:book_cover],
+      cover_file_name: create_params[:cover_file_name],
+      original_stock: create_params[:original_stock],
+      current_stock: create_params[:current_stock],
+      description: create_params[:description],
+      book_type: create_params[:book_type],
+      price: create_params[:price],
+      author_id: create_params[:author_id],
+      category_id: create_params[:category_id],
+      genre_id: create_params[:genre_id],
+      language_id: create_params[:language_id])
 
-      if book.valid?
-        render json: { message: "Book added successfully" }, status: :created
-      else
-        render json: { message: "Failed to add new book", error: book.errors }, status: :unprocessable_entity
-      end
-
-    rescue ActionController::UnpermittedParameters => e
-      # See create_book_params for allowed parameters
-      render_invalid_parameters(e)
-
-    rescue StandardError => e
-      render json: { message: "Internal server error", error: e.message }, status: :internal_server_error
+    if book.valid?
+      render json: { message: "Book added successfully" }, status: :created
+    else
+      render json: { message: "Failed to add new book", error: book.errors }, status: :unprocessable_entity
     end
   end
 
@@ -51,7 +42,7 @@ class Api::Admin::BookManagementController < ApplicationController
 
   private
 
-  def create_book_params
-    params.require(:book_management).permit(:title, :publication_year, :isbn, :book_cover, :cover_file_name, :original_stock, :current_stock, :description, :book_type, :price, :author_id, :category_id, :genre_id, :language_id)
+  def create_params
+    params.require(:data).permit(:title, :publication_year, :isbn, :book_cover, :cover_file_name, :original_stock, :current_stock, :description, :book_type, :price, :author_id, :category_id, :genre_id, :language_id)
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  rescue_from StandardError, with: :render_standard_error
+  rescue_from ActionController::UnpermittedParameters, with: :render_invalid_parameters
   private
 
   def render_invalid_parameters(exception)
@@ -8,5 +10,9 @@ class ApplicationController < ActionController::API
     invalid_parameters = error.flatten
     message = "Found #{invalid_parameters.length} invalid parameters: [#{invalid_parameters.join(", ")}]"
     render json: { message: "Invalid parameters", error: message }, status: :unprocessable_entity
+  end
+
+  def render_standard_error(exception)
+    render json: { message: "Internal server error", error: exception.message }, status: :internal_server_error
   end
 end
