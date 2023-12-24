70638e8c56d260545e4f603511bd2348caf82b93
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-12-24 12:54:17 +0700

    feat: unique name index on masterdata

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_23_155655) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_24_053100) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -38,18 +38,21 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_23_155655) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index "lower((name)::text)", name: "index_author_name_unique_lowercase", unique: true
   end
 
   create_table "book_category", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index "lower((name)::text)", name: "index_category_name_unique_lowercase", unique: true
   end
 
   create_table "book_genre", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index "lower((name)::text)", name: "index_genre_name_unique_lowercase", unique: true
   end
 
   create_table "borrowed_book", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -68,6 +71,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_23_155655) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index "lower((name)::text)", name: "index_language_name_unique_lowercase", unique: true
   end
 
   create_table "sold_book", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
