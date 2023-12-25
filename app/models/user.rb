29bf58ac516560452af6418ac93a56d0e16b185c
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-12-25 21:29:34 +0700

    feat: add enum checking on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   validates :email, presence: { message: "Email is required" }, uniqueness: { message: "Email already exists" }
   validates :username, presence: { message: "Username is required" }, uniqueness: { message: "Username already exists" }
   validates :password, presence: { message: "Password is required" }, length: { minimum: 8 }
+  validates :role, inclusion: %w[ADMIN CUSTOMER]
 
   has_one :user_profile
 end
--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -2,6 +2,7 @@
 
 class UserProfile < ApplicationRecord
   validates :first_name, presence: { message: "First name is required" }
+  validates :gender, inclusion: %w[MALE FEMALE]
 
   belongs_to :user
   has_one :user_address
