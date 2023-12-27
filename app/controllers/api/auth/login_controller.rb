bdf63ede7427bc0d474f4efbb601ac16b0c29a62
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-12-27 20:30:06 +0700

    feat: refresh token + update jwt payload

--- app/controllers/api/auth/login_controller.rb
+++ app/controllers/api/auth/login_controller.rb
@@ -2,15 +2,23 @@
 require 'jwt'
 
 class Api::Auth::LoginController < ApplicationController
+  skip_before_action :authenticate_user
+
   def create
     # Find user based on username
     # Encode JWT token once found
     user = User.find_by(username: login_params[:username])
 
     if user && user.authenticate(login_params[:password])
-      payload = { id: user.id, role: user.role }
-      token = JWT.encode(payload, ENV["JWT_SECRET_KEY"] || "SECRET", "HS256")
-      render json: { message: "Login successful", token: token }, status: :ok
+      # Access token expiry: 5 minutes
+      # Refresh token expiry: 7 days
+
+      access_token_payload = { id: user.id, role: user.role, exp: Time.now.to_i + 300, token_type: "ACCESS" }
+      refresh_token_payload = { id: user.id, role: user.role, exp: Time.now.to_i + (7 * 60 * 60 * 24), token_type: "REFRESH" }
+
+      access_token = JWT.encode(access_token_payload, ENV["JWT_ACCESS_SECRET"], "HS256", { typ: "JWT" })
+      refresh_token = JWT.encode(refresh_token_payload, ENV["JWT_REFRESH_SECRET"], "HS256", { typ: "JWT" })
+      render json: { message: "Login successful", access_token: access_token, refresh_token: refresh_token }, status: :ok
     else
       render_invalid_credentials
     end
