6f45c3ae7c7f94fe6e11d10de6124d9ff228da01
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-12-27 20:29:49 +0700

    feat: user and admin middleware

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,37 @@
+require 'jwt'
+
 class ApplicationController < ActionController::API
+  before_action :authenticate_user
   rescue_from StandardError, with: :render_standard_error
   rescue_from ActionController::UnpermittedParameters, with: :render_invalid_parameters
+
   private
+
+  def authenticate_user
+    bearer_token = request.headers["Authorization"]
+
+    if bearer_token.blank?
+      return render json: { message: "Authorization token is required" }, status: :unauthorized
+    end
+    token = bearer_token.split(" ")[1]
+
+    begin
+      decoded = JWT.decode(token, ENV["JWT_ACCESS_SECRET"], true, { required_claims: %w[id role exp], verify_expiration: true, algorithm: "HS256" })[0]
+      @role = decoded["role"]
+    rescue JWT::ExpiredSignature
+      return render json: { message: "Session expired. Please login to continue" }, status: :unauthorized
+    rescue JWT::MissingRequiredClaim || JWT::InvalidSignature
+      return render json: { message: "Token invalid. Please login to continue" }, status: :unauthorized
+    rescue StandardError => e
+      return render_standard_error(e)
+    end
+  end
+
+  def authenticate_admin
+    if @role == "CUSTOMER"
+      render json: { message: "Access denied" }, status: :forbidden
+    end
+  end
 
   def render_valid_create(item)
     render json: { message: "#{item} created successfully" }, status: :created
