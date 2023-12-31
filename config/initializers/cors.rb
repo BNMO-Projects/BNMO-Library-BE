161b6ecea72cd7f1ca2beddb8e2e5c60b74d9208
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-12-31 23:05:01 +0700

    fix: cors from netlify app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins "http://localhost:5173", "https://bnmo-library-customer.netlify.app"
 
     resource "*",
       headers: :any,
