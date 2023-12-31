78d2bb4a525f4b1ece4b621c5aeb6ce73ebd3d32
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-12-31 21:51:06 +0700

    feat: add env reading to database yml
    also fix validation issue

--- Gemfile
+++ Gemfile
@@ -42,7 +42,7 @@ gem 'jwt'
 gem 'faker'
 
 # Dotenv
-gem 'dotenv-rails', groups: :development
+gem 'dotenv-rails'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/sold_book.rb
+++ app/models/sold_book.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SoldBook < ApplicationRecord
-  validates :purchase_date, { message: "Purchase date is required" }
+  validates :purchase_date, presence: { message: "Purchase date is required" }
   validates :price, presence: { message: "Price is required" }
 
   belongs_to :book
