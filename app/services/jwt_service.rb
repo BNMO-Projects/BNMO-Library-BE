e36bfd916e0d3279b53e1a8c725811c235142856
YakobusIP <yakobusiryantoprasethio@gmail.com>
2024-01-05 00:03:10 +0700

    feat: auth service objects + jwt

---
+++ app/services/jwt_service.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class JwtService
+  def self.encode_access_token(user)
+    payload = { id: user.id, role: user.role, exp: Time.now.to_i + 300, token_type: "ACCESS" }
+    JWT.encode(payload, ENV["JWT_ACCESS_SECRET"], "HS256", { typ: "JWT" })
+  end
+
+  def self.encode_refresh_token(user)
+    payload = { id: user.id, role: user.role, exp: Time.now.to_i + 300, token_type: "REFRESH" }
+    JWT.encode(payload, ENV["JWT_REFRESH_SECRET"], "HS256", { typ: "JWT" })
+  end
+
+  def self.decode_token(token, secret_key)
+    JWT.decode(token, secret_key, true, { required_claims: %w[id role exp token_type], verify_expiration: true, algorithm: "HS256" })[0]
+  end
+end
