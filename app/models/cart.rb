12d55263de8c08164b9709cd409fc7ef3a7275df
YakobusIP <yakobusiryantoprasethio@gmail.com>
2024-01-05 10:32:55 +0700

    feat: cart item create + service object

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Cart < ApplicationRecord
-  validates :status, presence: { message: "Status is required" }, inclusion: %w[PENDING SUCCESS]
+  validates :status, presence: { message: "is required" }, inclusion: %w[ACTIVE COMPLETED]
 
   belongs_to :user
   has_many :cart_items
---
+++ app/services/cart_item_create_service.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class CartItemCreateService < BaseServiceObject
+  def initialize(user_id, book_id)
+    super()
+    @user_id = user_id
+    @book_id = book_id
+  end
+
+  def call
+    # Fetch the user's active cart (noted by status PENDING)
+    # If not found, create a new cart
+    # Once cart is made, check the book stock availability
+    # If available, create the cart item
+    cart = Cart.where(status: "ACTIVE").select("carts.id").find_or_create_by(user_id: @user_id)
+    book = Book.find_by_id(@book_id)
+
+    if book.current_stock.nonzero?
+      item = CartItem.create(book_id: @book_id, cart_id: cart.id, price: book.price)
+
+      if item.valid?
+        self.result = { item: item }
+      else
+        self.errors = item.errors.full_messages
+      end
+    else
+      self.errors = ["Book is out of stock"]
+    end
+
+    self
+  end
+end
